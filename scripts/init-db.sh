e46152a3a106ee1c6b42aaf5b9c5395402c1239c
Jeromie <jeromie@cinderlabs.ai>
2026-05-30 18:11:34 -0700

    postgres: init-db.sh + healthcheck must specify --dbname postgres (was defaulting to user-named db that doesn't exist)

--- scripts/init-db.sh
+++ scripts/init-db.sh
@@ -3,7 +3,7 @@
 # Creates the two product databases used by Strapi and Postiz.
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE strapi;
     CREATE DATABASE postiz;
 EOSQL
